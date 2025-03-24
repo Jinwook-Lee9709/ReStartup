014d43a7b2bf6631900e288c5bf1af501aaa78da
ddodo <ddodo1997@naver.com>
2025-03-24 10:57:23 +0900

    2025-03-24 일간보고용 테스트코드 추가

--- Assets/01_Scripts/SceneSpecific/Game/WorkTest.cs
+++ Assets/01_Scripts/SceneSpecific/Game/WorkTest.cs
@@ -7,11 +7,12 @@ public class WorkTest : MonoBehaviour
     [SerializeField] private WorkManager workManager;
     [SerializeField] WorkerManager workerManager;
     [SerializeField] Table interactableObject;
+    [SerializeField] [Range(1f,5f)] float testDuration = 1f;
 
     [ContextMenu("Assign Work")]
     public void AssingWork()
     {
-        InteractWorkBase cleanWorkBase = new WorkGetOrder(workManager, WorkType.Hall, 1f);
+        InteractWorkBase cleanWorkBase = new WorkGetOrder(workManager, WorkType.Hall, testDuration);
         cleanWorkBase.SetInteractable(interactableObject);
         interactableObject.SetWork(cleanWorkBase);
         workManager.AddWork(cleanWorkBase);
