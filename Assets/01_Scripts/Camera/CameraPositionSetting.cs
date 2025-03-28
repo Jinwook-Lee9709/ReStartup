6482136d498afab4932da41f8a0178e94ee77b9b
Jinwook-Lee9709 <wooki9709@gmail.com>
2025-03-28 09:29:17 +0900

    카메라 위치계산 수정

--- Assets/01_Scripts/Camera/CameraPositionSetting.cs
+++ Assets/01_Scripts/Camera/CameraPositionSetting.cs
@@ -1,10 +1,31 @@
+using System;
 using UnityEngine;
 
 public class CameraPositionSetting : MonoBehaviour
 {
+    enum CameraPosition
+    {
+        Hall,
+        Kitchen
+    }
+    
+    [SerializeField] private CameraPosition cameraPosition;
     private void Start()
     {
-        var screenWidth = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 1.5f, Screen.height * 0.5f, 0f));
-        transform.position = screenWidth;
+        var leftPos = Camera.main.ScreenToWorldPoint(new Vector3(0f, Screen.height * 0.5f, 0f));
+        var rightPos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height * 0.5f, 0f));
+        
+        var screenWorldWidth = rightPos.x - leftPos.x;
+
+        switch (cameraPosition)
+        {
+            case CameraPosition.Hall:
+                transform.position = new Vector3(-screenWorldWidth * 0.5f, transform.position.y, transform.position.z);
+                break;
+            case CameraPosition.Kitchen:
+                transform.position = new Vector3(screenWorldWidth * 0.5f, transform.position.y, transform.position.z);
+                break;
+        }
+            
     }
 }
