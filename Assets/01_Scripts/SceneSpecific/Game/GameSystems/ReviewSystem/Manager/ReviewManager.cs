c44f1955051827c97c7cf24a35eb2bf2f5f8fea1
ddodo <ddodo1997@naver.com>
2025-04-01 10:48:48 +0900

    2025-04-01 일간보고 커밋
    손님 테이블 적용, 손님 타입별 색 설정, 리뷰 작업중(리뷰 매니저의 컨텍스트 메뉴로 리뷰 생성 테스트 가능)

---
+++ Assets/01_Scripts/SceneSpecific/Game/GameSystems/ReviewSystem/Manager/ReviewManager.cs
@@ -0,0 +1,113 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ReviewManager : MonoBehaviour
+{
+    public LinkedList<Review> reviews = new LinkedList<Review>();
+    public GameObject reviewContent;
+    public GameObject reviewPrefab;
+
+    private readonly int maxReviewCnt = 20;
+
+    private void Awake()
+    {
+        DOTween.Init();
+        //for (int i = 0; i < maxReviewCnt; i++)
+        //{
+        //    AddBestReview();
+        //}
+    }
+
+    public void AddReview(bool isBest)
+    {
+        //ReviewData tempData = new();
+        //tempData.Init(isBest);
+        //var reviewObj = Instantiate(reviewPrefab).GetComponent<Review>();
+        //reviewObj.Init(tempData);
+
+        //reviews.Add(reviewObj);
+        //UpdateReviews();
+
+
+        if (isBest)
+            AddBestReview();
+        else
+            AddWorstReview();
+    }
+
+    [ContextMenu("Add Best Review")]
+    public void AddBestReview()
+    {
+        ReviewData tempData = new();
+        tempData.Init(true);
+        var gameObj = Instantiate(reviewPrefab);
+        gameObj.transform.SetParent(reviewContent.transform);
+        gameObj.transform.SetSiblingIndex(0);
+
+
+
+        var reviewObj = gameObj.GetComponent<Review>();
+        reviewObj.Init(tempData);
+
+        UserDataManager.Instance.CurrentUserData.CurrentRankPoint += 10;
+
+        reviews.AddFirst(reviewObj);
+
+        if (reviews.Count > maxReviewCnt)
+        {
+            RemoveAt(reviews.Last());
+        }
+
+        UpdateReviews();
+    }
+
+    [ContextMenu("Add Worst Review")]
+    public void AddWorstReview()
+    {
+        ReviewData tempData = new();
+        tempData.Init(false);
+        var gameObj = Instantiate(reviewPrefab);
+        gameObj.transform.SetParent(reviewContent.transform);
+        gameObj.transform.SetSiblingIndex(0);
+
+        var reviewObj = gameObj.GetComponent<Review>();
+        reviewObj.Init(tempData);
+
+        UserDataManager.Instance.CurrentUserData.CurrentRankPoint -= 10;
+
+        reviews.AddFirst(reviewObj);
+
+        if (reviews.Count > maxReviewCnt)
+        {
+            RemoveAt(reviews.Last());
+        }
+
+        UpdateReviews();
+    }
+
+    public void RemoveAt(Review removeReview)
+    {
+        removeReview.GetComponent<RectTransform>().DOScale(0f, 1f).SetEase(Ease.InOutElastic).OnComplete(() =>
+        {
+            Destroy(removeReview.gameObject);
+            reviews.Remove(removeReview);
+            UpdateReviews();
+        });
+    }
+
+    public void UpdateReviews()
+    {
+        int idx = 0;
+        foreach (var review in reviews)
+        {
+            review.GetComponent<RectTransform>().DOAnchorPos(new Vector2(0, idx * -review.GetComponent<RectTransform>().sizeDelta.y), 1f).SetEase(Ease.InOutCubic);
+            idx++;
+        }
+        var contentSize = reviewContent.GetComponent<RectTransform>().sizeDelta;
+        reviewContent.GetComponent<RectTransform>().sizeDelta = new Vector2(contentSize.x, reviewPrefab.GetComponent<RectTransform>().sizeDelta.y * idx);
+    }
+
+}
