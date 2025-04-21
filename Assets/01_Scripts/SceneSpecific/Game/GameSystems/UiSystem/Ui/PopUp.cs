1aa3317f2e94f99d40905b3df534197f055fbda1
ddodo <ddodo1997@naver.com>
2025-04-21 09:37:13 +0900

    2025-04-21
    피드백 적용 / 팝업 연속터치 방지

--- Assets/01_Scripts/SceneSpecific/Game/GameSystems/UiSystem/Ui/PopUp.cs
+++ Assets/01_Scripts/SceneSpecific/Game/GameSystems/UiSystem/Ui/PopUp.cs
@@ -10,12 +10,14 @@ public abstract class PopUp : MonoBehaviour
     [SerializeField] protected GameObject popupUi;
     protected virtual void Start()
     {
-        popupUi.transform.DOScale(1f, 0.5f).SetEase(Ease.InOutElastic);
+        backGround.interactable = false;
+        popupUi.transform.DOScale(1f, 0.5f).SetEase(Ease.InOutElastic).OnComplete(() => backGround.interactable = true);
         backGround.onClick.AddListener(OnCancle);
     }
 
     protected virtual void OnCancle()
     {
+        backGround.interactable = false;
         popupUi.transform.DOScale(0f, 0.5f).SetEase(Ease.InOutElastic).OnComplete(() =>
         {
             Destroy(gameObject);
